12dddbd16b8ea9afc96499fac789156582bcdb3f
Stuart Lynne <stuart.lynne@gmail.com>
2021-08-26 12:15:12 -0700

    interim

--- new-master.sh
+++ new-master.sh
@@ -5,5 +5,5 @@ sudo rm -rfv postgresql-data/*
 ./manage.sh start
 docker exec -it racedb_8080 python3 /RaceDB/manage.py flush
 docker exec -it racedb_8080 python3 /RaceDB/manage.py loaddata /racedb-data/test.json
-./manage.sh restart
+#./manage.sh restart
 
