d1debfef963f1dcd44b96b4ee2775ea220453ea1
Stuart Lynne <stuart.lynne@gmail.com>
2024-02-26 19:02:13 -0800

    ee

--- scripts/start-portainer.sh
+++ scripts/start-portainer.sh
@@ -4,5 +4,5 @@ set -x
 docker volume create portainer_data
 docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data portainer/portainer-ce:latest
+    -v portainer_data:/data portainer/portainer-ee:latest
 
