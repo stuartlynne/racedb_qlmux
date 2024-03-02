9ec082896e307c84b2ebba877ed90eabb27efcef
Stuart Lynne <stuart.lynne@gmail.com>
2024-03-01 17:00:05 -0800

    super and lpr

--- racedb/94-setsuper.sh
+++ racedb/94-setsuper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -x
+#set -x
 cd /RaceDB
-python manage.py set_password super racedb
+#python manage.py set_password super racedb
+python manage.py set_password super super
+python manage.py set_password reg reg
 
