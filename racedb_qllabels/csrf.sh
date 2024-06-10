40ea4fffbe3f14425052da75b282b1314af1607b
root <stuart.lynne@gmail.com>
2024-06-10 12:07:02 -0700

    convert csrf.sh to csrf.py

--- racedb_qllabels/csrf.sh
+++ racedb_qllabels/csrf.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 #
-echo CSRF_TRUSTED_ORIGINS = [\"${CSRF_TRUSTED_ORIGINS}\", ]  >> /RaceDB/RaceDB/settings.py
+#echo CSRF_TRUSTED_ORIGINS = [\"${CSRF_TRUSTED_ORIGINS}\", ]  >> /RaceDB/RaceDB/settings.py
+#env
+echo
+echo --
+echo $0: $*
+echo --
+echo CSRF_TRUSTED_ORIGINS = [\"${CSRF_TRUSTED_ORIGINS}\", ]  
