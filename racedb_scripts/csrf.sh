7d18c83b99820deb0d6a8494514329a8bb1844c4
root <stuart.lynne@gmail.com>
2024-09-04 13:43:45 -0700

    lpr and csrf.py cannot by symlinks

--- racedb_scripts/csrf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-#echo CSRF_TRUSTED_ORIGINS = [\"${CSRF_TRUSTED_ORIGINS}\", ]  >> /RaceDB/RaceDB/settings.py
-#env
-echo
-echo --
-echo $0: $*
-echo --
-echo CSRF_TRUSTED_ORIGINS = [\"${CSRF_TRUSTED_ORIGINS}\", ]  
--- racedb_ssh/csrf.py
+++ racedb_ssh/csrf.py
@@ -1,1 +1,17 @@
-../racedb_scripts/csrf.py
+#!/usr/local/bin/python
+
+import sys
+import os
+
+try:
+    CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+except Exception as e:
+    print('Error:', e)
+    sys.exit(1)
+
+print()
+print('# CSRF_TRUSTED_ORIGINS is required by django to allow an https proxy to work correctly')
+print('# This is added by /csrf.py during docker-compose up')
+print('# See the docker.env file in the build directory for the container to customize')
+print('CSRF_TRUSTED_ORIGINS = [\"%s", ]' % '\", \"'.join(CSRF_TRUSTED_ORIGINS))  
+print()
